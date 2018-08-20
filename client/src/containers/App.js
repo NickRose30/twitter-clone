b2e8fbf470db4ebabef41d8b712529cb5aca2122
NickRose30 <nicholasrose30@gmail.com>
2018-08-20 18:49:30 -0400

    initial navbar code

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { configureStore } from '../store';
 import { BrowserRouter as Router } from 'react-router-dom';
+import Navbar from './Navbar';
 
 /** Create our store to be passed to our Provider component. */
 const store = configureStore();
@@ -9,7 +10,7 @@ const store = configureStore();
 const App = () => (
   <Provider store={store}>
     <Router>
-      <h1>Hello World</h1>
+      <Navbar />
     </Router>
   </Provider>
 );
