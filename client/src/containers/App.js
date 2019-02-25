17cab1e1b39f55b2b154ebc4fd2a316572e81d89
NickRose30 <nicholasrose30@gmail.com>
2019-02-25 17:49:31 -0500

    cleanup

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -3,7 +3,7 @@ import { Provider } from 'react-redux';
 import { configureStore } from '../store';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Main from './Main';
-import {setAuthorizationToken, setCurrentUser } from "../store/actions/auth";
+import { setAuthorizationToken, setCurrentUser } from "../store/actions/auth";
 import jwtDecode from 'jwt-decode';
 
 /** Create our store to be passed to our Provider component. */
